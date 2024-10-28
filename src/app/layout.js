47337ff2637831e2bc9f915518e411ae572f2c84
KimPuro <puro@protonmail.ch>
2024-10-29 04:23:01 +0900

    feat: 메타데이터 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import {Analytics} from "@vercel/analytics/react";
 
 export const metadata = {
   title: "BANANA ICE",
-  description: "BANANA ICE",
+  description: "BANANA ICE, an Asian video production company, mainly produces and uploads videos related to Korean hip-hop",
 };
 
 export default function RootLayout({ children }) {

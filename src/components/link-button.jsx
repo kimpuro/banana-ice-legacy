89d7f31eeba1ca77e2cbfee6f98418db75ed920e
KimPuro <puro@protonmail.ch>
2024-10-29 04:10:52 +0900

    feat: 링크를 새 창에서 열리게 변경

--- src/components/link-button.jsx
+++ src/components/link-button.jsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 export default function LinkButton({buttonType}) {
     if (buttonType === "youtube") {
         return (
-            <Link href={"https://www.youtube.com/@BANANAICE/"}>
+            <Link href={"https://www.youtube.com/@BANANAICE/"} target="_blank" rel="noopener noreferrer">
                 <Image src="/icons/youtube.png" alt="youtube link image" width={40} height={40}/>
             </Link>
         )
     }
     if (buttonType === "insta") {
         return (
-            <Link href={"https://www.instagram.com/kithbill/"}>
+            <Link href={"https://www.instagram.com/kithbill/"} target="_blank" rel="noopener noreferrer">
                 <Image src="/icons/insta.png" alt="insta link image" width={40} height={40}/>
             </Link>
         )
